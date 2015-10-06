c4ecffd5c89a6dfe4f2af8509a6c397896b3bb4b
Yamikuronue <yamikuronue@gmail.com>
2015-10-06 15:36:07 -0400

    Some styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 var Hapi = require('hapi');
+var path = require('path');
 
 var server = new Hapi.Server();
 server.connection({ port: 3000 });
 
 
-server.register(require('vision'), function (err) {
+server.register([require('vision'), require('inert')], function (err) {
 
 	server.views({
 	    engines: {
@@ -20,6 +21,28 @@ server.register(require('vision'), function (err) {
 	    handler: function (request, reply) {
 	        reply.view('home');
 	    }
+	});
+
+	server.route({
+	    method: 'GET',
+	    path: '/img/{param*}',
+	    handler: {
+	    	directory: {
+            	path: path.resolve(__dirname,'img'),
+            	listing: true
+            }
+        }
+	});
+
+	server.route({
+	    method: 'GET',
+	    path: '/static/{param*}',
+	    handler: {
+	    	directory: {
+            	path: path.resolve(__dirname,'static'),
+            	listing: true
+            }
+        }
 	});
 });
 
